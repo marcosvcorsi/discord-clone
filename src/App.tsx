1745794ee4f5ea5aa5723ee1b18ff717c4109712
Marcos Corsi <marcosvcorsi@gmail.com>
2020-07-10 22:24:40 -0300

    Server List added

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import GlobalStyles from "./styles/GlobalStyles";
 import Layout from "./components/Layout";
 
-function App() {
+const App = () => {
   return (
     <>
       <GlobalStyles />
       <Layout />
     </>
   );
-}
+};
 
 export default App;
